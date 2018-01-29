55f88540f5e40ee1c8e7fef1286a300345d318ce
Lozamded <dhiguerafernandez@gmail.com>
2018-01-29 00:45:35 -0300

    usuario en el welcome

--- db/schema.rb
+++ db/schema.rb
@@ -74,7 +74,6 @@ ActiveRecord::Schema.define(version: 20180125172534) do
     t.bigint "precio"
     t.bigint "precio_sruka"
     t.string "precio_costo"
-    t.string "bigint"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
