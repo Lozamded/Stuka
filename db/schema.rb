1f401de2fb62ce06ac956c10db1648d8e9be7d9a
Lozamded <dhiguerafernandez@gmail.com>
2018-03-10 00:49:29 -0300

    imagen test para heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219173558) do
+ActiveRecord::Schema.define(version: 20180220022641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,7 +128,6 @@ ActiveRecord::Schema.define(version: 20180219173558) do
 
   create_table "propietarios", force: :cascade do |t|
     t.string "nombre"
-    t.string "rut"
     t.date "fecha_nacimiento"
     t.string "email"
     t.string "telefono"
@@ -136,6 +135,7 @@ ActiveRecord::Schema.define(version: 20180219173558) do
     t.string "comuna"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "rut"
     t.string "picture"
   end
 
@@ -165,6 +165,7 @@ ActiveRecord::Schema.define(version: 20180219173558) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "name", default: "", null: false
+    t.string "type", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -196,7 +197,6 @@ ActiveRecord::Schema.define(version: 20180219173558) do
 
   add_foreign_key "boleta", "consulta", column: "consulta_id"
   add_foreign_key "con_ins", "consulta"
-  add_foreign_key "con_procs", "consulta"
   add_foreign_key "con_vets", "consulta"
   add_foreign_key "consulta", "perros"
   add_foreign_key "consulta", "procedimientos"
